0a89a8f2695b3ab5a167e864f978c12c91cdef23
Oleg Klimenko <me@ctizen.net>
2018-01-20 21:49:36 +0300

    PNTN-220 Replace manual seating with interval seating

--- Mimir/src/controllers/Seating.php
+++ Mimir/src/controllers/Seating.php
@@ -119,6 +119,47 @@ class SeatingController extends Controller
     }
 
     /**
+     * Make new interval seating.
+     * This will also start games immediately if timer is not used.
+     *
+     * @param int $eventId
+     * @param int $step
+     * @throws AuthFailedException
+     * @throws DatabaseException
+     * @throws InvalidParametersException
+     * @throws InvalidUserException
+     * @return bool
+     */
+    public function makeIntervalSeating($eventId, $step)
+    {
+        $this->_checkIfAllowed($eventId);
+        $this->_log->addInfo('Creating new interval seating for event #' . $eventId);
+        $gamesWillStart = $this->_updateEventStatus($eventId);
+
+        $event = EventPrimitive::findById($this->_db, [$eventId]);
+        if (empty($event)) {
+            throw new InvalidParametersException('Event id#' . $eventId . ' not found in DB');
+        }
+
+        $currentRatingTable = (new EventModel($this->_db, $this->_config, $this->_meta))
+            ->getRatingTable($event[0], 'rating', 'desc');
+
+        $seating = Seating::makeIntervalSeating($currentRatingTable, $step, true);
+
+        $tableIndex = 1;
+        foreach ($seating as $table) {
+            (new InteractiveSessionModel($this->_db, $this->_config, $this->_meta))
+                ->startGame($eventId, $table, $tableIndex++); // TODO: here might be an exception inside loop!
+        }
+
+        $this->_log->addInfo('Created new interval seating for event #' . $eventId);
+        if ($gamesWillStart) {
+            $this->_log->addInfo('Started all games by interval seating for event #' . $eventId);
+        }
+        return true;
+    }
+
+    /**
      * Update event "seating ready" status.
      * This should be done before games start, or admin panel will show some inadequate data.
      * @param $eventId
