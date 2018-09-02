974a739015df5dfdd587aa7705213df02ae773f7
Oleg Klimenko <me@ctizen.net>
2018-09-02 17:11:27 +0300

    Models unit tests skeleton

---
+++ Frey/tests/models/AccountTest.php
@@ -0,0 +1,51 @@
+<?php
+/*  Frey: ACL & user data storage
+ *  Copyright (C) 2018  o.klimenko aka ctizen
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace Frey;
+
+require_once __DIR__ . '/../../src/models/Account.php';
+require_once __DIR__ . '/../../src/Db.php';
+
+class AccountModelTest extends \PHPUnit_Framework_TestCase
+{
+    protected $_db;
+
+    public function setUp()
+    {
+        $this->_db = Db::__getCleanTestingInstance();
+    }
+
+    public function testCreateAccount()
+    {
+
+    }
+
+    public function testGetPersonalInfo()
+    {
+
+    }
+
+    public function testUpdatePersonalInfo()
+    {
+
+    }
+
+    public function testFindByTitleFuzzy()
+    {
+
+    }
+}
