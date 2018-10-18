fe3ea2a04421209a9a55f67bfc3832c76d3ec83c
Oleg Klimenko <me@ctizen.net>
2018-10-18 20:45:19 +0300

    Add bootstrapper to initial migration

--- Frey/data/migrations/20180318160552_init_frey_version1m0.php
+++ Frey/data/migrations/20180318160552_init_frey_version1m0.php
@@ -2,6 +2,8 @@
 // @codingStandardsIgnoreFile
 use Phinx\Migration\AbstractMigration;
 
+require_once __DIR__ . '/../../src/helpers/BootstrapAccess.php';
+
 class InitFreyVersion1m0 extends AbstractMigration
 {
     public function up()
@@ -14,6 +16,19 @@ class InitFreyVersion1m0 extends AbstractMigration
         $this->_genPersonGroup();
 //        $this->_genFormation();
 //        $this->_genFormationUser();
+
+        $this->getAdapter()->commitTransaction();
+
+        list($db, $config) = $this->_getConnection();
+        $meta = new \Frey\Meta($_SERVER);
+        try {
+            \Frey\BootstrapAccess::create(
+                $db, $config, $meta,
+                'admin@pantheon.mahjong', 'changethis'
+            );
+        } catch (\Exception $e) {
+            echo 'Failed to bootstrap superadmin & supergroup: ' . $e->getMessage();
+        }
     }
 
     /**
@@ -146,6 +161,32 @@ class InitFreyVersion1m0 extends AbstractMigration
             ->save();
     }
 
+    protected function _getConnection()
+    {
+        $cfg = new \Frey\Config([
+            'db' => [
+                'connection_string' => 'pgsql:host=localhost;port=' . $_SERVER['PHINX_DB_FREY_PORT']
+                    . 'dbname=' . $_SERVER['PHINX_DB_FREY_NAME'],
+                'credentials' => [
+                    'username' => $_SERVER['PHINX_DB_FREY_USER'],
+                    'password' => $_SERVER['PHINX_DB_FREY_PASS']
+                ]
+            ],
+            'admin'     => [
+                'debug_token' => '2-839489203hf2893'
+            ],
+            'routes'    => require __DIR__ . '/../../config/routes.php',
+            'verbose'   => false,
+            'verboseLog' => '',
+            'api' => [
+                'version_major' => 1,
+                'version_minor' => 0
+            ],
+            'testing_token' => 'not_used_here'
+        ]);
+
+        return [new \Frey\Db($cfg), $cfg];
+    }
 
 //    /**
 //     * Local clubs, leagues, etc
