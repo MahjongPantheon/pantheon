09469def21c417c09accc88bb53a47c454f1c881
Oleg Klimenko <oleg.klimenko@team.wrike.com>
2019-03-29 23:49:39 +0300

    Wip: frey client gen interface

--- Frey/bin/clientGen.php
+++ Frey/bin/clientGen.php
@@ -18,60 +18,123 @@
 namespace Frey;
 
 if (empty($argv[1])) {
-    trigger_error('Wrong subsystem argument' . PHP_EOL . 'Usage: php bin/clientGen.php Mimir' . PHP_EOL, E_USER_ERROR);
+    trigger_error('Wrong subsystem argument' . PHP_EOL . 'Usage: php bin/clientGen.php Mimir [interface]' . PHP_EOL, E_USER_ERROR);
     die();
 }
 
-// require all controllers
-$dir = opendir(__DIR__ . '/../src/controllers/');
-while (($file = readdir($dir))) {
-    if ($file == '.' || $file == '..') {
-        continue;
-    }
-    require_once __DIR__ . '/../src/controllers/' . $file;
+if (!empty($argv[2]) && $argv[2] == 'interface') {
+    genInterface(getData(), $argv[1]);
+} else {
+    genClient(getData(), $argv[1]);
 }
 
-$routes = require __DIR__ . '/../config/routes.php';
+function getData() {
+    // require all controllers
+    $dir = opendir(__DIR__ . '/../src/controllers/');
+    while (($file = readdir($dir))) {
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
+        require_once __DIR__ . '/../src/controllers/' . $file;
+    }
 
-$doc = [];
+    $routes = require __DIR__ . '/../config/routes.php';
 
-foreach ($routes as $methodName => $callable) {
-    $classRefl = new \ReflectionClass('\\Frey\\' . $callable[0]);
-    $method = $classRefl->getMethod($callable[1]);
-    $docComment = explode("\n", $method->getDocComment());
-    $doc[$methodName] = [
-        'comment'       => [],
-        'params'        => [],
-        'exceptions'    => [],
-        'return'        => []
-    ];
+    $doc = [];
 
-    foreach ($docComment as $line) {
-        if (preg_match('#@param\s+(?<type>\S+)\s+(?<name>\\$\S+)(\s+(?<comment>.+))?#is', $line, $typedParams)) {
-            $doc[$methodName]['params'] []= $typedParams;
-        } else if (preg_match('#@throws\s+(?<type>\S+)(\s+(?<comment>.+))?#is', $line, $exceptions)) {
-            $doc[$methodName]['exceptions'] []= $exceptions;
-        } else if (preg_match('#@return\s+(?<type>\S+)(\s+(?<comment>.+))?#is', $line, $return)) {
-            $doc[$methodName]['return'] = $return;
-        } else if (trim($line) != '/**' && trim($line) != '*/') {
-            $doc[$methodName]['comment'] []= preg_replace('#^\s+\*#', '', $line);
+    foreach ($routes as $methodName => $callable) {
+        $classRefl = new \ReflectionClass('\\Frey\\' . $callable[0]);
+        $method = $classRefl->getMethod($callable[1]);
+        $docComment = explode("\n", $method->getDocComment());
+        $doc[$methodName] = [
+            'comment'       => [],
+            'params'        => [],
+            'exceptions'    => [],
+            'return'        => []
+        ];
+
+        foreach ($docComment as $line) {
+            if (preg_match('#@param\s+(?<type>\S+)\s+(?<name>\\$\S+)(\s+(?<comment>.+))?#is', $line, $typedParams)) {
+                $doc[$methodName]['params'] []= $typedParams;
+            } else if (preg_match('#@throws\s+(?<type>\S+)(\s+(?<comment>.+))?#is', $line, $exceptions)) {
+                $doc[$methodName]['exceptions'] []= $exceptions;
+            } else if (preg_match('#@return\s+(?<type>\S+)(\s+(?<comment>.+))?#is', $line, $return)) {
+                $doc[$methodName]['return'] = $return;
+            } else if (trim($line) != '/**' && trim($line) != '*/') {
+                $doc[$methodName]['comment'] []= preg_replace('#^\s+\*#', '', $line);
+            }
         }
     }
+
+    return $doc;
 }
 
+function genInterface($doc, $packageName) {
+    ?><?php echo '<?php' . PHP_EOL; ?>
+
+namespace <?php echo $packageName; ?>;
+
+/**
+* Interface IFreyClient
+* THIS IS A GENERATED FILE! DO NOT MODIFY BY HAND, USE bin/clientGen.php
+*
+* @package <?php echo $packageName; ?>
+*/
+interface IFreyClient
+{
+
+public function __construct(string $apiUrl);
+
+/**
+* @returns \JsonRPC\Client
+*/
+public function getClient();
+
+<?php foreach ($doc as $methodName => $method):
+
+    $typedParams = [];
+    $phpDocParams = [];
+    $plainParams = [];
+
+    foreach ($method['params'] as $param) {
+        $phpDocParams []= "@param {$param['type']} {$param['name']}";
+        $typedParams []= (strpos($param['type'], '|') !== false ? '' : $param['type'] . ' ') . $param['name'];
+        $plainParams []= $param['name'];
+    }
+
+    ?>
+
+    /**<?php if (!empty($method['comment'])) echo PHP_EOL . '     * ' . implode("\n     * ", array_filter($method['comment'])); ?>
+
+    <?php if (!empty($phpDocParams)) echo ' * ' . implode("\n     * ", $phpDocParams); ?>
+
+    <?php if (!empty($method['return'])) echo ' * @returns ' . $method['return']['type']; ?>
+
+    */
+    public function <?php echo $methodName; ?>(<?php
+
+    echo implode(', ', $typedParams);
+    ?>);
+<?php endforeach; ?>
+}
+<?php
+}
+
+function genClient($doc, $packageName) {
 ?><?php echo '<?php' . PHP_EOL; ?>
 
-namespace <?php echo $argv[1]; ?>;
+namespace <?php echo $packageName; ?>;
 
 require_once __DIR__ . '/HttpClient.php'; // TODO: replace with custom jsonrpc httpclient implementation path
+require_once __DIR__ . '/interfaces/IFreyClient.php'; // TODO: replace with custom frey client interface path
 
 /**
  * Class FreyClient
  * THIS IS A GENERATED FILE! DO NOT MODIFY BY HAND, USE bin/clientGen.php
  *
- * @package <?php echo $argv[1]; ?>
+ * @package <?php echo $packageName; ?>
  */
-class FreyClient
+class FreyClient implements IFreyClient
 {
     /**
     * @var \JsonRPC\Client
@@ -121,4 +184,6 @@ class FreyClient
             echo $methodName; ?>', [<?php echo implode(', ', $plainParams); ?>]);
     }
 <?php endforeach; ?>
+}
+<?php
 }
