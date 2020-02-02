c11ae4cf10ca5cb07cce574ee05b9514bd5cc51f
Galina Kapger <gikapger@gmail.com>
2020-02-03 01:15:45 +0500

    fix yandex metrika

--- Tyr/src/app/app.component.ts
+++ Tyr/src/app/app.component.ts
@@ -44,27 +44,6 @@ export class AppComponent {
     private themeService: ThemeService
   ) {
 
-    // Yandex metrika init code; Don't touch :)
-    (function (m, e, t, r, i, k, a) {
-      m[i] = m[i] || function () {
-        (m[i].a = m[i].a || []).push(arguments)
-      };
-      m[i].l = 1 * +(new Date());
-      k = e.createElement(t),
-        a = e.getElementsByTagName(t)[0],
-        k.async = 1,
-        k.src = r,
-        a.parentNode.insertBefore(k, a)
-    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-    window.ym(environment.metrikaId, "init", {
-      id: environment.metrikaId,
-      defer: true,
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true
-    });
-    // End of Yandex metrika
-
     this.state = new AppState(
       this.zone,
       this.api,
