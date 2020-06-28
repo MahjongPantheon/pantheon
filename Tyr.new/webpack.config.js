59301546a4572475e500ca4ed9bc5c4e357d4358
Oleg Klimenko <me@ctizen.dev>
2020-06-28 19:31:32 +0300

    Base configs

---
+++ Tyr.new/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/index.tsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '#': path.resolve(__dirname, 'app')
+    },
+    extensions: [ '.tsx', '.ts', '.js', '.jsx' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
