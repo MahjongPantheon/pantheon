21c8f836cd793638d42c93b86818940e167465ff
Oleg Klimenko <me@ctizen.dev>
2021-10-08 22:33:17 +0300

    Fix migration

--- Mimir/data/migrations/20211008011544_slight_cleanup.php
+++ Mimir/data/migrations/20211008011544_slight_cleanup.php
@@ -6,8 +6,15 @@ class SlightCleanup extends AbstractMigration
 {
     public function change()
     {
-        $this->table('__deprecated_formation')->drop();
+        $this->table('event')
+            ->dropForeignKey('owner_formation')
+            ->removeIndex(['owner_formation'])
+            ->removeColumn('__deprecated_owner_formation')
+            ->removeColumn('__deprecated_owner_player')
+            ->removeColumn('__deprecated_type')
+            ->save();
         $this->table('__deprecated_formation_player')->drop();
+        $this->table('__deprecated_formation')->drop();
         $this->table('__deprecated_player')->drop();
         $this->table('event_registered_players')
             ->removeColumn('auth_token')
