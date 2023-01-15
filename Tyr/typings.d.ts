97cef1b96696ab0ab905f85287e8963be98ca659
Gin Kapger <gikapger@gmail.com>
2023-01-15 22:56:41 +0100

    Configure loader

--- Tyr/typings.d.ts
+++ Tyr/typings.d.ts
@@ -2,3 +2,11 @@ interface Window {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
   ym: (counterId: number, funcName: string, param1: any, param2?: any) => void;
 }
+
+declare module '*.svg?svgr' {
+  import React from 'react'
+
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  // eslint-disable-next-line import/no-default-export
+  export default SVG
+}
