a61bf3b14d6c5e6ade83645c12364261f62f8f9c
Oleg Klimenko <me@ctizen.dev>
2023-06-19 02:35:38 +0300

    Add license info

--- Hugin/www/twirp/index.php
+++ Hugin/www/twirp/index.php
@@ -1,5 +1,20 @@
 <?php
-
+/*  Hugin: system statistics
+ *  Copyright (C) 2023  o.klimenko aka ctizen
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../../Common/generated/Common/HuginServer.php';
 require __DIR__ . '/../../src/TwirpServer.php';
