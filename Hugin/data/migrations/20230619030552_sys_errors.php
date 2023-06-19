e924a0fe03df554acb2d8857afa9bc85c354d941
Oleg Klimenko <me@ctizen.dev>
2023-06-19 03:50:54 +0300

    Use bigints for id

--- Hugin/data/migrations/20230619030552_sys_errors.php
+++ Hugin/data/migrations/20230619030552_sys_errors.php
@@ -11,8 +11,9 @@ class SysErrors extends AbstractMigration
 
     protected function _genSysErrors()
     {
-        $table = $this->table('sys_errors');
+        $table = $this->table('sys_errors', ['id' => false, 'primary_key' => 'id']);
         $table
+            ->addColumn('id', 'biginteger', ['identity' => true, 'signed' => false])
             ->addColumn('source', 'string', ['limit' => 255])
             ->addColumn('created_at', 'datetime')
             ->addColumn('error', 'text')
