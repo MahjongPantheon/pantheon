9eabf436a232f7b14ac2374609244d761d052f07
Oleg Klimenko <me@ctizen.dev>
2023-07-02 13:34:43 +0300

    Fix envs and shell prompts

--- Sigrun/entrypoint.sh
+++ Sigrun/entrypoint.sh
@@ -2,6 +2,7 @@
 
 export HOME=/home/user
 cat /env/"$ENV_FILENAME" | grep -v '# ' | grep -v  -e '^$' | awk '{ print "export " $1 }' > /etc/profile.d/external.sh
+echo "export PS1=\"|\033[1;31m Sigrun container \033[0m~> \$PWD (\\u) \\$ \"" >> /etc/profile.d/external.sh
 . /etc/profile.d/external.sh
 
 # yarn care
