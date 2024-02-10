1a8b310e56fed7238d722c7249c2c4a4bb45d6a3
Oleg Klimenko <me@ctizen.dev>
2024-02-10 18:57:49 +0100

    Add pwa for tyr

--- Tyr/vite.config.ts
+++ Tyr/vite.config.ts
@@ -19,9 +19,43 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()],
+  plugins: [
+    svgr(),
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,gif}'],
+        cleanupOutdatedCaches: false,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Tyr game tracker',
+        short_name: 'PantheonTracker',
+        description: 'Mahjong pantheon assistant app',
+        theme_color: '#cccccc',
+        icons: [
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'apple-touch-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 4003,
   },
