a30206f7591b0d89b87fd576b3c9522593d98e7d
Oleg Klimenko <me@ctizen.dev>
2024-02-12 00:52:47 +0300

    Use compression for backups

--- Database/cron/backup.sh
+++ Database/cron/backup.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 cd ~ || exit
 rm -rf ~/backup/frey ~/backup/mimir
 pg_dump --clean -Z0 -Fd frey -f ~/backup/frey
 pg_dump --clean -Z0 -Fd mimir -f ~/backup/mimir
+pg_dump --clean -Z0 -Fd hugin -f ~/backup/hugin
 cd /var/lib/postgresql/backup || exit
+
+tar -c --lzma -f frey.tar.xz frey
+rm -rf frey
+tar -c --lzma -f mimir.tar.xz mimir
+rm -rf mimir
+tar -c --lzma -f hugin.tar.xz hugin
+rm -rf hugin
 
 git add .
 git commit -m "Backup @ `date`"
@@ -16,4 +24,12 @@ else
   git remote set-url origin $BACKUP_GIT_REMOTE
 fi
 
-git push origin main
+git push origin main 2>~/gitpush.log
+if [ $? -eq 0 ]; then
+  echo "Success"
+else
+  echo "Backup failed!"
+  echo -e "Pantheon backup has failed. Here is what git said: \n\n" > ~/message.txt
+  cat ~/gitpush.log >> ~/message.txt
+  mail -s "[CRITICAL] Pantheon backup failed" $ADMIN_EMAIL < ~/message.txt
+fi
