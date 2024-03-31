43d0ad264ca3ea8759ea079c2a1b903064418973
Oleg Klimenko <me@ctizen.dev>
2024-03-31 23:13:19 +0300

    Add vite-pwa-plugin to sigrun/forseti

--- Sigrun/vite.config.ts
+++ Sigrun/vite.config.ts
@@ -18,9 +18,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,gif,woff2}'],
+        cleanupOutdatedCaches: false,
+      },
+      includeAssets: ['apple-touch-icon.png'],
+      manifest: {
+        name: 'Sigrun ratings viewer',
+        short_name: 'PantheonViewer',
+        description: 'Mahjong pantheon ratings viewer',
+        theme_color: '#cccccc',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 4002,
   },
