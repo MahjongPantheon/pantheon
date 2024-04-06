473d792050cb0bb87cef6a1e2dfea0ddd088b6ac
Oleg Klimenko <me@ctizen.dev>
2024-04-06 20:23:00 +0300

    Add Fenrir container

---
+++ Fenrir/README.md
@@ -0,0 +1,12 @@
+## Fenrir: Pantheon e2e testing tool
+
+Goals:
+- e2e testing
+- performance testing
+- try to set things on fire as much as possible
+
+Fenrir uses Playwright as testing framework.
+
+#### Legend
+
+Fenrir plays a key role during the events of Ragnarök, where he is foretold to assist in setting the world aflame, resulting in the collapse of humanity and society.
---
+++ Fenrir/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+export HOME=/home/user
+cat /env/"$ENV_FILENAME" | grep -v '# ' | grep -v  -e '^$' | awk '{ print "export " $1 }' > /etc/profile.d/external.sh
+echo "export PS1=\"|\033[1;31m Fenrir container \033[0m~> \$PWD (\\u) \\$ \"" >> /etc/profile.d/external.sh
+. /etc/profile.d/external.sh
+
+# yarn care
+mkdir /home/user/.yarn-cache
+chown user /home/user/.yarn-cache
+
+munin-node
+
+TRAPPED_SIGNAL=false
+
+echo 'Starting NGINX';
+nginx -g "daemon off;" 2>&1 &
+NGINX_PID=$!
+
+trap "TRAPPED_SIGNAL=true; kill -15 $NGINX_PID;" SIGTERM  SIGINT
+
+while :
+do
+    kill -0 $NGINX_PID 2> /dev/null
+    NGINX_STATUS=$?
+
+    if [ "$TRAPPED_SIGNAL" = "false" ]; then
+        if [ $NGINX_STATUS -ne 0 ]; then
+            if [ $NGINX_STATUS -eq 0 ]; then
+                kill -15 $NGINX_PID;
+                wait $NGINX_PID;
+            fi
+            exit 1;
+        fi
+    else
+       if [ $NGINX_STATUS -ne 0 ]; then
+            exit 0;
+       fi
+    fi
+
+    sleep 1
+done
+
