1bf04e0ee9c4e0b5a46ca2dfb06aae3acaed2b49
Oleg Klimenko <me@ctizen.dev>
2025-07-19 03:14:46 +0300

    basic migration

---
+++ Frey/app/migrate.ts
@@ -0,0 +1,49 @@
+import * as path from 'path';
+import { Pool } from 'pg';
+import { promises as fs } from 'fs';
+import { Kysely, Migrator, PostgresDialect, FileMigrationProvider } from 'kysely';
+import { Database } from './schema';
+import { env } from './helpers/env';
+
+async function migrateToLatest() {
+  const db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        host: env.db.host,
+        database: env.db.dbname,
+        user: env.db.username,
+        password: env.db.password,
+      }),
+    }),
+  });
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      // This needs to be an absolute path.
+      migrationFolder: path.join(__dirname, './migrations'),
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error('failed to migrate');
+    console.error(error);
+    process.exit(1);
+  }
+
+  await db.destroy();
+}
+
+await migrateToLatest();
---
+++ Frey/app/migrations/initial-2025-07-19-02-38.ts
@@ -0,0 +1,122 @@
+import { Kysely, sql } from 'kysely'
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('person')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('title', 'varchar', (col) => col.notNull())
+    .addColumn('email', 'varchar', (col) => col.notNull())
+    .addColumn('auth_hash', 'varchar', (col) => col.notNull())
+    .addColumn('auth_salt', 'varchar', (col) => col.notNull())
+    .addColumn('auth_reset_token', 'varchar')
+    .addColumn('country', 'varchar', (col) => col.notNull().defaultTo(''))
+    .addColumn('city', 'varchar')
+    .addColumn('phone', 'varchar')
+    .addColumn('telegram_id', 'varchar')
+    .addColumn('tenhou_id', 'varchar')
+    .addColumn('disabled', 'integer', (col) => col.notNull().defaultTo(0))
+    .addColumn('has_avatar', 'integer')
+    .addColumn('last_update', "timestamp")
+    .addColumn('is_superadmin', 'integer')
+    .addColumn('notifications', 'text')
+    .execute()
+
+  await db.schema
+    .createIndex('person_country')
+    .on('person')
+    .column('country')
+    .execute()
+
+  await db.schema
+    .createIndex('person_disabled')
+    .on('person')
+    .column('disabled')
+    .execute()
+
+  await db.schema
+    .createIndex('person_email')
+    .on('person')
+    .column('email')
+    .execute()
+
+  await db.schema
+    .createIndex('person_title')
+    .on('person')
+    .column('title')
+    .execute()
+
+  await db.schema
+    .createIndex('titlesearch_idx')
+    .expression(sql`USING GIN (to_tsvector('simple', title))`)
+    .execute()
+
+  await db.schema
+    .createTable('person_access')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('acl_name', 'varchar', (col) => col.notNull())
+    .addColumn('acl_value', 'integer', (col) => col.notNull())
+    .addColumn('person_id', 'integer', (col) => col.references('person.id').onDelete('cascade').notNull())
+    .addColumn('event_id', 'integer')
+    .execute()
+
+  await db.schema
+    .createIndex('person_access_acl_name')
+    .on('person_access')
+    .column('acl_name')
+    .execute()
+
+  await db.schema
+    .createIndex('person_access_event_id')
+    .on('person_access')
+    .column('event_id')
+    .execute()
+
+  await db.schema
+    .createTable('registrant')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('email', 'varchar', (col) => col.notNull())
+    .addColumn('auth_hash', 'varchar', (col) => col.notNull())
+    .addColumn('auth_salt', 'varchar', (col) => col.notNull())
+    .addColumn('approval_code', 'varchar', (col) => col.notNull())
+    .addColumn('title', 'varchar', (col) => col.notNull())
+    .execute()
+
+  await db.schema
+    .createIndex('registrant_approval_code')
+    .on('registrant')
+    .column('approval_code')
+    .execute()
+
+  await db.schema
+    .createIndex('registrant_email')
+    .on('registrant')
+    .column('email')
+    .execute()
+
+  await db.schema
+    .createTable('majsoul_platform_accounts')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('person_id', 'integer', (col) => col.references('person.id').onDelete('cascade').notNull())
+    .addColumn('nickname', 'varchar', (col) => col.notNull())
+    .addColumn('account_id', 'integer', (col) => col.notNull())
+    .addColumn('friend_id', 'integer', (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createIndex('majsoul_platform_accounts_account_id')
+    .on('majsoul_platform_account')
+    .column('account_id')
+    .unique()
+    .execute()
+
+  await db.schema
+    .createIndex('majsoul_platform_accounts_nickname')
+    .on('majsoul_platform_account')
+    .column('nickname')
+    .execute()
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('pet').execute()
+  await db.schema.dropTable('person').execute()
+}
