a9111399e208a02fff40b5b30c2bd3a84f441d33
Oleg Klimenko <me@ctizen.dev>
2025-07-30 00:44:47 +0300

    fix test run

--- Frey/jest.config.js
+++ Frey/jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
+  reporters: process.env.GITHUB_ACTIONS
+    ? [['github-actions', {silent: false}], 'summary']
+    : [['default', {summaryThreshold: 10}]],
   ...(process.env.DEBUG
     ? {}
     : {
