daa6fa58d3bea40955843610de9a025cd75d8659
Oleg Klimenko <me@ctizen.dev>
2025-08-03 00:24:58 +0300

    fix migration

--- Frey/app/database/migrate_from_frey1.ts
+++ Frey/app/database/migrate_from_frey1.ts
@@ -1,4 +1,4 @@
-import { Kysely, PostgresDialect } from 'kysely';
+import { Kysely, PostgresDialect, sql } from 'kysely';
 import { DB as DatabaseOld } from './schema_v1';
 import { Pool } from 'pg';
 import { env } from '../helpers/env';
@@ -26,6 +26,7 @@ export async function migrateFromFrey1() {
     let i = 0;
     const limit = 100;
     console.log('Migrating person table');
+    let personLastId = 0;
     while (true) {
       const records = await oldDb
         .selectFrom('person')
@@ -35,30 +36,36 @@ export async function migrateFromFrey1() {
         .offset(i)
         .execute();
       if (records.length === 0) {
+        await sql`select setval('person_id_seq', ${personLastId + 1})`.execute(trx);
         break;
       }
 
       await trx
         .insertInto('person')
         .values(
-          records.map((rec) => ({
-            id: rec.id,
-            auth_hash: rec.auth_hash,
-            auth_reset_token: rec.auth_reset_token,
-            auth_salt: rec.auth_salt,
-            city: rec.city,
-            country: rec.country,
-            disabled: rec.disabled,
-            email: rec.email,
-            has_avatar: rec.has_avatar,
-            is_superadmin: rec.is_superadmin,
-            last_update: rec.last_update,
-            notifications: rec.notifications,
-            phone: rec.phone,
-            telegram_id: rec.telegram_id,
-            tenhou_id: rec.tenhou_id,
-            title: rec.title,
-          }))
+          records.map((rec) => {
+            if (rec.id > personLastId) {
+              personLastId = rec.id;
+            }
+            return {
+              id: rec.id,
+              auth_hash: rec.auth_hash,
+              auth_reset_token: rec.auth_reset_token,
+              auth_salt: rec.auth_salt,
+              city: rec.city,
+              country: rec.country,
+              disabled: rec.disabled,
+              email: rec.email,
+              has_avatar: rec.has_avatar,
+              is_superadmin: rec.is_superadmin,
+              last_update: rec.last_update,
+              notifications: rec.notifications,
+              phone: rec.phone,
+              telegram_id: rec.telegram_id,
+              tenhou_id: rec.tenhou_id,
+              title: rec.title,
+            };
+          })
         )
         .execute();
       process.stdout.write('.');
@@ -68,6 +75,7 @@ export async function migrateFromFrey1() {
 
     i = 0;
     console.log('Migrating person_access table');
+    let personAccessLastId = 0;
     while (true) {
       const recordsAll = await oldDb
         .selectFrom('person_access')
@@ -78,6 +86,7 @@ export async function migrateFromFrey1() {
         .execute();
 
       if (recordsAll.length === 0) {
+        await sql`select setval('person_access_id_seq', ${personAccessLastId + 1})`.execute(trx);
         break;
       }
 
@@ -95,13 +104,18 @@ export async function migrateFromFrey1() {
       await trx
         .insertInto('person_access')
         .values(
-          records.map((rec) => ({
-            acl_name: rec.acl_name,
-            acl_value: rec.acl_value === 'true' ? 1 : 0,
-            event_id: rec.event_id ?? -1,
-            id: rec.id,
-            person_id: rec.person_id,
-          }))
+          records.map((rec) => {
+            if (rec.id > personAccessLastId) {
+              personAccessLastId = rec.id;
+            }
+            return {
+              acl_name: rec.acl_name,
+              acl_value: rec.acl_value === 'true' ? 1 : 0,
+              event_id: rec.event_id ?? -1,
+              id: rec.id,
+              person_id: rec.person_id,
+            };
+          })
         )
         .execute();
 
@@ -112,6 +126,7 @@ export async function migrateFromFrey1() {
 
     i = 0;
     console.log('Migrating majsoul_platform_accounts table');
+    let accLastId = 0;
     while (true) {
       const records = await oldDb
         .selectFrom('majsoul_platform_accounts')
@@ -121,19 +136,25 @@ export async function migrateFromFrey1() {
         .offset(i)
         .execute();
       if (records.length === 0) {
+        await sql`select setval('majsoul_platform_account_id_seq', ${accLastId + 1})`.execute(trx);
         break;
       }
 
       await trx
         .insertInto('majsoul_platform_account')
         .values(
-          records.map((rec) => ({
-            id: rec.id,
-            account_id: rec.account_id,
-            friend_id: rec.friend_id,
-            nickname: rec.nickname,
-            person_id: rec.person_id,
-          }))
+          records.map((rec) => {
+            if (rec.id > accLastId) {
+              accLastId = rec.id;
+            }
+            return {
+              id: rec.id,
+              account_id: rec.account_id,
+              friend_id: rec.friend_id,
+              nickname: rec.nickname,
+              person_id: rec.person_id,
+            };
+          })
         )
         .execute();
 
@@ -144,6 +165,7 @@ export async function migrateFromFrey1() {
 
     i = 0;
     console.log('Migrating registrant table');
+    let regLastId = 0;
     while (true) {
       const records = await oldDb
         .selectFrom('registrant')
@@ -153,20 +175,26 @@ export async function migrateFromFrey1() {
         .offset(i)
         .execute();
       if (records.length === 0) {
+        await sql`select setval('registrant_id_seq', ${regLastId + 1})`.execute(trx);
         break;
       }
 
       await trx
         .insertInto('registrant')
         .values(
-          records.map((rec) => ({
-            id: rec.id,
-            approval_code: rec.approval_code,
-            auth_hash: rec.auth_hash,
-            auth_salt: rec.auth_salt,
-            email: rec.email,
-            title: rec.title,
-          }))
+          records.map((rec) => {
+            if (rec.id > regLastId) {
+              regLastId = rec.id;
+            }
+            return {
+              id: rec.id,
+              approval_code: rec.approval_code,
+              auth_hash: rec.auth_hash,
+              auth_salt: rec.auth_salt,
+              email: rec.email,
+              title: rec.title,
+            };
+          })
         )
         .execute();
 
