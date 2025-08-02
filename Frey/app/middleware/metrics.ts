ded19392eefda0f2001ea950ef37b43615538dca
Oleg Klimenko <me@ctizen.dev>
2025-08-02 23:32:43 +0300

    fix error logging

--- Frey/app/middleware/metrics.ts
+++ Frey/app/middleware/metrics.ts
@@ -2,23 +2,29 @@ import { Middleware } from 'twirpscript';
 import { Context } from '../context';
 import { IncomingMessage } from 'http';
 import { env } from '../helpers/env';
+import { Logger } from 'simple-node-logger';
 
-export function metrics(): Middleware<Context, IncomingMessage> {
+export function metrics(logger: Logger): Middleware<Context, IncomingMessage> {
   return async (req, ctx, next) => {
     const time = performance.now();
-    const result = await next();
-    const duration = performance.now() - time;
-    await fetch(env.huginUrl + '/addMetric', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify([
-        {
-          m: 'method_exec_duration_' + req.url?.replace('/v2/common.Frey/', ''),
-          v: duration,
-          s: 'frey',
-        },
-      ]),
-    });
-    return result;
+    try {
+      const result = await next();
+      const duration = performance.now() - time;
+      await fetch(env.huginUrl + '/addMetric', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify([
+          {
+            m: 'method_exec_duration_' + req.url?.replace('/v2/common.Frey/', ''),
+            v: duration,
+            s: 'frey',
+          },
+        ]),
+      });
+      return result;
+    } catch (e: any) {
+      logger.error('Request errored', req.url?.replace('/v2/common.Frey/', '') ?? 'Unknown', e);
+      throw e;
+    }
   };
 }
--- Frey/app/server.ts
+++ Frey/app/server.ts
@@ -1,5 +1,6 @@
 import { createServer, IncomingMessage } from 'http';
 import { createTwirpServer } from 'twirpscript';
+import { createSimpleLogger } from 'simple-node-logger';
 import { createFrey } from './clients/proto/frey.pb';
 import { freyClient } from './frey';
 import { env } from './helpers/env';
@@ -7,13 +8,12 @@ import { Context } from './context';
 import { fillRequestVars } from './middleware/requestVars';
 import { createDbConstructor, createRedisConstructor } from './database/db';
 import { storages } from './middleware/storages';
-import { Log } from './helpers/log';
 import { metrics } from './middleware/metrics';
 
 export const freyHandler = [createFrey(freyClient)];
 const dbConstructor = createDbConstructor();
 const redisConstructor = createRedisConstructor();
-const logger = new Log('/var/log/frey.log');
+const logger = createSimpleLogger('/var/log/frey.log');
 
 redisConstructor().then((redis) => {
   const app = createTwirpServer<Context, typeof freyHandler, IncomingMessage>(freyHandler, {
@@ -22,18 +22,18 @@ redisConstructor().then((redis) => {
   })
     .use(fillRequestVars())
     .use(storages(dbConstructor(), redis))
-    .use(metrics());
+    .use(metrics(logger));
 
   app.on('requestReceived', (ctx) => {
-    logger.logStart(ctx.method?.name ?? 'Unknown', []);
+    logger.info('Request received', ctx.method?.name ?? 'Unknown');
   });
 
   app.on('responseSent', (ctx) => {
-    logger.logSuccess(ctx.method?.name ?? 'Unknown', []);
+    logger.info('Response sent', ctx.method?.name ?? 'Unknown');
   });
 
   app.on('error', (ctx, err) => {
-    logger.logFailure(ctx.method?.name ?? 'Unknown', [err.msg]);
+    logger.error('Request errored', ctx.method?.name ?? 'Unknown', err);
   });
 
   createServer(app).listen(env.port, () => console.log(`Server listening on port ${env.port}`));
