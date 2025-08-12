8c218ffdc96b5bed3ac9ed2e504dfe6446ddf1fd
Oleg Klimenko <me@ctizen.dev>
2025-08-12 21:11:37 +0300

    remove mimir v1

--- Mimir/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export HOME=/home/user
-echo "export PS1=\"|\033[1;31m Mimir container \033[0m~> \$PWD (\\u) \\$ \"" > /etc/profile.d/external.sh
-
-# -l 0 for verbosity
-crond -b -l 8 -L /tmp/cronlogs
-
-exec supervisord -c /etc/supervisord.conf
