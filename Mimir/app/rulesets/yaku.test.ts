10baee69415f0948d7b8707732ab84cbc84bac08
Oleg Klimenko <me@ctizen.dev>
2025-08-14 01:17:23 +0300

    rulesets

---
+++ Mimir/app/rulesets/yaku.test.ts
@@ -0,0 +1,14 @@
+import { fromTenhou, Yaku } from './yaku';
+
+describe('Yaku', () => {
+  it('should convert properly from tenhou ids', () => {
+    const yakuList = '1,1,2,1,24,2,52,2,53,1,54,3';
+    const yakumanList = '45';
+    expect(fromTenhou(yakuList, yakumanList)).toEqual({
+      yaku: [Yaku.RIICHI, Yaku.IPPATSU, Yaku.ITTSU, Yaku.CHUURENPOUTO],
+      yakuman: 1,
+      dora: 6,
+      han: -1,
+    });
+  });
+});
