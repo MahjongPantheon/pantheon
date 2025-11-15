c4d0bcf60fedc9deaf196cec22d3b5ca5ae3f947
Oleg Klimenko <me@ctizen.dev>
2025-11-16 02:41:38 +0300

    fix frey cron

--- Frey/entrypoint.sh
+++ Frey/entrypoint.sh
@@ -3,4 +3,7 @@
 export HOME=/home/user
 echo "export PS1=\"|\033[1;31m Frey container \033[0m~> \$PWD (\\u) \\$ \"" > /etc/profile.d/external.sh
 
+# -l 0 for verbosity
+crond -b -l 8 -L /tmp/cronlogs
+
 exec supervisord -c /etc/supervisord.conf
