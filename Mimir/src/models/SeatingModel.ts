e84495532c6861d5eca4c5b68cf9a5d3676c2589
Oleg Klimenko <me@ctizen.dev>
2026-05-12 02:19:44 +0300

    makeShuffledSeating

--- Mimir/src/models/SeatingModel.ts
+++ Mimir/src/models/SeatingModel.ts
@@ -4,7 +4,19 @@ import { EventEntity } from 'src/entities/Event.entity.js';
 import { PlayerModel } from './PlayerModel.js';
 import { PlayerHistoryModel } from './PlayerHistoryModel.js';
 import { PlayerInSessionModel } from './PlayerInSessionModel.js';
-import { EventsGetCurrentSeatingResponse } from 'tsclients/proto/mimir.pb.js';
+import { make_seating_shuffled } from 'mahjong-seatings-rs-node';
+import {
+  EventsGetCurrentSeatingResponse,
+  SeatingMakeShuffledSeatingPayload,
+} from 'tsclients/proto/mimir.pb.js';
+import {
+  GenericSuccessResponse,
+  SessionStatus,
+  TournamentGamesStatus,
+} from 'tsclients/proto/atoms.pb.js';
+import { SessionModel } from './SessionModel.js';
+import { EventRegistrationModel } from './EventRegistrationModel.js';
+import { PenaltyModel } from './PenaltyModel.js';
 
 export class SeatingModel extends Model {
   public async getCurrentSeating(eventId: number): Promise<EventsGetCurrentSeatingResponse> {
@@ -60,5 +72,151 @@ export class SeatingModel extends Model {
         }))
         .filter((s) => !!s.table_index),
     };
+  }
+
+  async makeShuffledSeating(
+    payload: SeatingMakeShuffledSeatingPayload
+  ): Promise<GenericSuccessResponse> {
+    const { eventId, groupsCount, seed /*windShuffleMode*/ } = payload;
+    await this._ensureActionAllowed(eventId);
+
+    const sessionModel = this.getModel(SessionModel);
+    const sessions = await sessionModel.findByEventAndStatus(
+      [eventId],
+      [SessionStatus.SESSION_STATUS_INPROGRESS]
+    );
+    if (sessions.length === 0) {
+      throw new Error('No active session found for this event');
+    }
+
+    const event = await this.repo.em.findOne(EventEntity, eventId);
+    if (!event) {
+      throw new Error(`Event #${eventId} not found`);
+    }
+    if (event.useTimer) {
+      event.gamesStatus = TournamentGamesStatus.TOURNAMENT_GAMES_STATUS_SEATING_READY;
+      this.repo.em.persist(event);
+    }
+    const regModel = this.getModel(EventRegistrationModel);
+    const regs = await regModel.findByEventId([event.id]);
+
+    const [playersMap, previousSeatings] = await this._getData(event, regs);
+    const seating = make_seating_shuffled({
+      playersMap,
+      groupsCount,
+      randFactor: seed,
+      // windShuffleMode, // TODO
+      previousSeatings,
+    });
+
+    const chunks = [];
+    for (let i = 0; i < seating.length; i += 4) {
+      chunks.push(seating.slice(i, i + 4).map((p) => p[0]));
+    }
+
+    const replacements: Record<number, number> = {};
+    for (const reg of regs) {
+      if (reg.replacementId) {
+        replacements[reg.playerId] = reg.replacementId;
+      }
+    }
+
+    let tableIndex = 1;
+    const promises = [];
+    for (const table of chunks) {
+      sessionModel.startSession(event, table, tableIndex);
+
+      for (const idx in table) {
+        if (replacements[table[idx]]) {
+          table[idx] = replacements[table[idx]];
+        }
+      }
+      promises.push(this.repo.skirnir.messageSeatingReady(table, tableIndex, eventId));
+      tableIndex++;
+    }
+
+    await this.repo.em.flush();
+    await Promise.all(promises);
+
+    return { success: true };
+  }
+
+  private async _ensureActionAllowed(eventId: number): Promise<void> {
+    const event = await this.repo.em.findOne(EventEntity, eventId);
+    if (!event) {
+      throw new Error(`Event #${eventId} not found`);
+    }
+
+    const playerModel = this.getModel(PlayerModel);
+    if (
+      !this.repo.meta.personId ||
+      !((await playerModel.isEventAdmin(eventId)) && (await playerModel.isEventReferee(eventId)))
+    ) {
+      throw new Error("You don't have the necessary permissions to make new seating");
+    }
+
+    if (event.finished) {
+      throw new Error('Event is already finished');
+    }
+  }
+
+  private async _getData(
+    event: EventEntity,
+    regs: EventRegisteredPlayersEntity[]
+  ): Promise<[Record<number, number>, number[][]]> {
+    const regModel = this.getModel(EventRegistrationModel);
+    const ignoredPlayerIds = await regModel.findIgnoredPlayersIdsByEvent([event.id]);
+
+    const penaltyModel = this.getModel(PenaltyModel);
+    const penalties = await penaltyModel.findByEventId([event.id], true);
+    const penaltyByPlayer = penalties.reduce(
+      (acc, penalty) => {
+        if (!acc[penalty.playerId]) {
+          acc[penalty.playerId] = { amount: 0, count: 0 };
+        }
+        acc[penalty.playerId].amount += penalty.amount;
+        acc[penalty.playerId].count++;
+        return acc;
+      },
+      {} as Record<number, { amount: number; count: number }>
+    );
+
+    const historyModel = this.getModel(PlayerHistoryModel);
+    const history = await historyModel.findLastByEvent([event.id]);
+    const playersMap: Record<number, number> = {};
+
+    // First step is adding players that already played games
+    for (const item of history) {
+      if (ignoredPlayerIds.includes(item.playerId)) {
+        continue;
+      }
+      playersMap[item.playerId] = item.rating - (penaltyByPlayer[item.playerId]?.amount ?? 0);
+    }
+
+    // Second step is adding players that didn't play yet
+    // this situation is possible only in online tournament
+    // when we added replacement player
+    const initialRating = event.ruleset?.rules.startRating ?? 0;
+    for (const reg of regs) {
+      if (ignoredPlayerIds.includes(reg.playerId)) {
+        continue;
+      }
+      if (!playersMap[reg.playerId]) {
+        playersMap[reg.playerId] = initialRating;
+      }
+    }
+
+    const playerInSessionModel = this.getModel(PlayerInSessionModel);
+    const previousSeatings = await playerInSessionModel.getPlayersSeatingInEvent(event.id);
+
+    const tables = previousSeatings
+      .map((s) => s.player_id)
+      .filter((id) => !ignoredPlayerIds.includes(id));
+    const seatingChunks: number[][] = [];
+    for (let i = 0; i < tables.length; i += 4) {
+      seatingChunks.push(tables.slice(i, i + 4));
+    }
+
+    return [playersMap, seatingChunks];
   }
 }
