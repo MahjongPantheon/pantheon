ded31fd16210b0f56cf09eaf0b0197b94b14872c
Oleg Klimenko <me@ctizen.dev>
2026-05-18 02:42:09 +0300

    debug build

--- Mimir/build.ts
+++ Mimir/build.ts
@@ -1,21 +1,30 @@
 import { build } from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 
-build({
-  entryPoints: ['src/server.ts'],
-  bundle: true,
-  platform: 'node',
-  format: 'esm',
-  target: 'node22',
-  outfile: 'dist/server.js',
-  plugins: [
-    nodeExternalsPlugin({
-      allowList: ['tsclients'],
-    }),
-  ],
-})
-  .then((result) => {
-    console.log(result);
+Promise.all([
+  build({
+    entryPoints: ['src/entities/**/*.entity.ts'],
+    platform: 'node',
+    format: 'esm',
+    target: 'node22',
+    outdir: 'dist/entities',
+  }),
+  build({
+    entryPoints: ['src/server.ts'],
+    bundle: true,
+    platform: 'node',
+    format: 'esm',
+    target: 'node22',
+    outfile: 'dist/server.js',
+    plugins: [
+      nodeExternalsPlugin({
+        allowList: ['tsclients'],
+      }),
+    ],
+  }),
+])
+  .then((results) => {
+    console.log(results);
   })
   .catch((err) => {
     console.error(err);
