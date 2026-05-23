c5e0a9cb53e028b82917ba190ae2efdf370dadc9
Oleg Klimenko <me@ctizen.dev>
2026-05-24 02:39:11 +0300

    mimir1 migrator - rounds

--- Mimir/src/migrate_from_mimir1.ts
+++ Mimir/src/migrate_from_mimir1.ts
@@ -3,6 +3,12 @@ import { Pool } from 'pg';
 import { Repository } from './services/Repository.js';
 import { MikroORM } from '@mikro-orm/postgresql';
 import config from './mikro-orm.config.js';
+import { RoundEntity } from './entities/Round.entity.js';
+import { HandEntity } from './entities/Hand.entity.js';
+import { EventEntity } from './entities/Event.entity.js';
+import { SessionEntity } from './entities/Session.entity.js';
+import { SessionStateEntity } from './entities/SessionState.entity.js';
+import { RoundOutcome } from 'tsclients/proto/atoms.pb.js';
 
 process.env.NODE_ENV = 'development';
 
@@ -41,6 +47,124 @@ export async function migrateFromMimir1() {
       lastId = await inserter(records, lastId);
       process.stdout.write('.');
       i += limit;
+    }
+  }
+
+  async function migrateRounds(em: MikroORM['em']) {
+    const limit = 10;
+    console.log('Migrating rounds into rounds/hands tables');
+    let lastId = 0;
+
+    function toOutcome(outcome: string): RoundOutcome {
+      switch (outcome) {
+        case 'ron':
+          return RoundOutcome.ROUND_OUTCOME_RON;
+        case 'tsumo':
+          return RoundOutcome.ROUND_OUTCOME_TSUMO;
+        case 'draw':
+          return RoundOutcome.ROUND_OUTCOME_DRAW;
+        case 'abort':
+          return RoundOutcome.ROUND_OUTCOME_ABORT;
+        case 'chombo':
+          return RoundOutcome.ROUND_OUTCOME_CHOMBO;
+        case 'nagashi':
+          return RoundOutcome.ROUND_OUTCOME_NAGASHI;
+        case 'multiron':
+          return RoundOutcome.ROUND_OUTCOME_MULTIRON;
+        default:
+          return RoundOutcome.ROUND_OUTCOME_UNSPECIFIED;
+      }
+    }
+
+    while (true) {
+      const sessionIds: number[] = (
+        await oldDb.query(`select id from session order by id asc limit ${limit} offset ${lastId}`)
+      ).rows.map((r) => +r.id);
+      lastId = Math.max(...sessionIds);
+      if (sessionIds.length === 0) {
+        break;
+      }
+
+      const rounds = (
+        await oldDb.query(
+          `select * from round where session_id in (${sessionIds.join(',')}) order by session_id asc, id asc limit ${limit} offset ${lastId}`
+        )
+      ).rows;
+      if (rounds.length === 0) {
+        continue;
+      }
+
+      const groupedRounds = [];
+      let lastMultiRound = [];
+
+      for (const round of rounds) {
+        if (round.multi_ron && round.multi_ron > 1) {
+          lastMultiRound.push(round);
+        } else {
+          if (lastMultiRound.length > 0) {
+            groupedRounds.push(lastMultiRound);
+            lastMultiRound = [];
+          }
+          groupedRounds.push([round]);
+        }
+      }
+
+      for (const group of groupedRounds) {
+        const r = new RoundEntity();
+        r.id = group[0].id;
+        r.event = em.getReference(EventEntity, group[0].event_id);
+        r.session = em.getReference(SessionEntity, group[0].session_id);
+        r.outcome = toOutcome(group[0].outcome);
+        r.round = group[0].round;
+        r.riichi = group[0].riichi ? group[0].riichi.split(',').map(Number) : [];
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+        const state = JSON.parse(group[0].last_session_state || '{}');
+        r.lastSessionState = new SessionStateEntity();
+        r.lastSessionState.round = state._round ?? 1;
+        r.lastSessionState.chips = state._chips ?? {};
+        r.lastSessionState.chombo = state._chombo ?? {};
+        r.lastSessionState.honba = state._honba ?? 0;
+        r.lastSessionState.lastHandStarted = state._lastHandStarted ?? false;
+        r.lastSessionState.prematurelyFinished = state._prematurelyFinished ?? false;
+        r.lastSessionState.roundJustChanged = state._roundJustChanged ?? true;
+        r.lastSessionState.lastOutcome = state._lastOutcome ?? '';
+        r.lastSessionState.yakitori = state._yakitori
+          ? Object.fromEntries(
+              state._playerIds.map((id: number, index: number) => [+id, !!state._yakitori[index]])
+            )
+          : {};
+        r.lastSessionState.replacements = state._replacements ?? {};
+        r.lastSessionState.playerIds = state._scores ? Object.keys(state._scores).map(Number) : [];
+        r.lastSessionState.riichiBets = state._riichiBets ?? 0;
+        r.lastSessionState.scores = state._scores ?? {};
+        r.honba = r.lastSessionState.honba;
+        rounds.push(r);
+
+        for (const rnd of group) {
+          const hand = new HandEntity();
+          hand.round = r;
+          hand.han = rnd.han ?? undefined;
+          hand.fu = rnd.fu ?? undefined;
+          hand.dora = rnd.dora;
+          hand.uradora = rnd.uradora;
+          hand.kandora = rnd.kandora;
+          hand.kanuradora = rnd.kanuradora;
+          hand.yaku = rnd.yaku ? rnd.yaku.split(',').map(Number) : [];
+          hand.tempai = rnd.tempai ? rnd.tempai.split(',').map(Number) : [];
+          hand.nagashi = rnd.nagashi ? rnd.nagashi.split(',').map(Number) : [];
+          hand.winnerId = rnd.winner_id ?? undefined;
+          hand.loserId = rnd.loser_id ?? undefined;
+          hand.paoPlayerId = rnd.pao_player_id ?? undefined;
+          hand.openHand = !!rnd.open_hand;
+          r.hands.push(hand);
+          em.persist(hand);
+        }
+
+        em.persist(r);
+      }
+
+      em.flush();
+      process.stdout.write('.');
     }
   }
 
@@ -303,37 +427,7 @@ export async function migrateFromMimir1() {
       }
     );
 
-    // TODO: посессионная миграция, выбираем 10 сессий и едем заполнять round и hand....
-    await migrateTable(
-      'round',
-      trx,
-      (limit, offset) => {
-        return oldDb
-          .selectFrom('round')
-          .orderBy('id', 'asc')
-          .selectAll()
-          .limit(limit)
-          .offset(offset)
-          .execute();
-      },
-      async (rows, lastId) => {
-        await trx
-          .insertInto('round')
-          .values(
-            rows.map((rec) => {
-              if (rec.id > lastId) {
-                lastId = rec.id;
-              }
-              return {
-                ...rec,
-                open_hand: rec.open_hand === 1 ? 1 : 0,
-              };
-            })
-          )
-          .execute();
-        return lastId;
-      }
-    );
+    await migrateRounds(em);
 
     await migrateTable(
       'session',
